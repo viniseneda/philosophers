0c7be5f60a17241ae709967f7ceca59f59b9765e
Vinicius <vvarussa@student.42sp.org.br>
2022-04-15 14:01:50 -0300

    update in try_to_eat

--- try_to_eat.c
+++ try_to_eat.c
@@ -6,7 +6,7 @@
 /*   By: vvarussa <vvarussa@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 11:44:56 by vvarussa          #+#    #+#             */
-/*   Updated: 2022/04/15 12:07:03 by vvarussa         ###   ########.fr       */
+/*   Updated: 2022/04/15 13:57:37 by vvarussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,35 +21,26 @@ void	set_fork(t_thread_data *data, int fork_index, int value)
 
 int	try_to_eat(t_thread_data *data)
 {
-	// if (data->name_of_thread % 2 == 1)
-	// 	usleep(10000);
 	pthread_mutex_lock(&data->forks_mutexes[data->left_index]);
 	if (data->forks[data->left_index])
 	{
-		// print_message("has taken a fork", *data);
 		data->forks[data->left_index] = 0;
 		pthread_mutex_unlock(&data->forks_mutexes[data->left_index]);
+		// print_message("has taken a fork", *data);
+		//usleep(100);
 		pthread_mutex_lock(&data->forks_mutexes[data->right_index]);
 		if (data->forks[data->right_index])
 		{
-			print_message("has taken a fork", *data);
-			print_message("has taken a fork", *data);
+
+			// print_message("has taken a fork", *data);
 			data->forks[data->right_index] = 0;
 			pthread_mutex_unlock(&data->forks_mutexes[data->right_index]);
 			print_message("is eating", *data);
 
-
 			gettimeofday(&data->thinking_time, NULL);
 			usleep(data->time_to_eat);
 
-			// pthread_mutex_lock(&data->forks_mutexes[data->left_index]);
-			// data->forks[data->left_index] = 1;
-			// pthread_mutex_unlock(&data->forks_mutexes[data->left_index]);
 			set_fork(data, data->left_index, 1);
-
-			// pthread_mutex_lock(&data->forks_mutexes[data->right_index]);
-			// data->forks[data->right_index] = 1;
-			// pthread_mutex_unlock(&data->forks_mutexes[data->right_index]);
 			set_fork(data, data->right_index, 1);
 
 			print_message("is sleeping", *data);
@@ -57,14 +48,10 @@ int	try_to_eat(t_thread_data *data)
 			return(1);
 		}
 		pthread_mutex_unlock(&data->forks_mutexes[data->right_index]);
-
-		// pthread_mutex_lock(&data->forks_mutexes[data->left_index]);
-		// data->forks[data->left_index] = 1;
-		// pthread_mutex_unlock(&data->forks_mutexes[data->left_index]);
 		set_fork(data, data->left_index, 1);
 		return (0);
 	}
 	pthread_mutex_unlock(&data->forks_mutexes[data->left_index]);
-	// gettimeofday(&data->thinking_time, NULL);
+	usleep(1000);
 	return (0);
 }
